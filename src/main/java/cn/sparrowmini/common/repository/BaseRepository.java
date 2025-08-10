652e5efba67caa529124a7487d966e6c6b317dab
Fan Sword <fanmeijian@gmail.com>
2025-08-10 21:43:59 +0800

    优化通用repository得方法，发布一个版本，准备下一个版本大改造。

--- src/main/java/cn/sparrowmini/common/repository/BaseRepository.java
+++ src/main/java/cn/sparrowmini/common/repository/BaseRepository.java
@@ -3,6 +3,8 @@ package cn.sparrowmini.common.repository;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.lang.NonNull;
+
 import java.util.Optional;
 
 @NoRepositoryBean
@@ -10,4 +12,13 @@ public interface BaseRepository<T, ID>
         extends JpaRepository<T, ID>, JpaSpecificationExecutor<T> {
 
      <S> Optional<S> findByIdProjection(ID id, Class<S> projectionClass);
+
+     @NonNull
+     Class<T> domainType();
+
+     @NonNull
+     Class<ID> idType();
+
+     @NonNull
+     String idFieldName();
 }
--- src/main/java/cn/sparrowmini/common/repository/BaseRepositoryImpl.java
+++ src/main/java/cn/sparrowmini/common/repository/BaseRepositoryImpl.java
@@ -1,16 +1,12 @@
 package cn.sparrowmini.common.repository;
+
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.Id;
-import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
-import org.springframework.data.jpa.repository.support.JpaEntityInformation;
-
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.lang.NonNull;
 
-import java.io.Serializable;
-import java.lang.reflect.Field;
 import java.util.Optional;
-import java.util.function.Function;
 
 public class BaseRepositoryImpl<T, ID>
         extends SimpleJpaRepository<T, ID>
@@ -18,30 +14,42 @@ public class BaseRepositoryImpl<T, ID>
 
     private final EntityManager em;
     private final Class<T> domainClass;
+    private final JpaEntityInformation<T, ID> entityInformation;
 
     public BaseRepositoryImpl(JpaEntityInformation<T, ?> entityInformation, EntityManager em) {
         super(entityInformation, em);
         this.domainClass = entityInformation.getJavaType();
         this.em = em;
+        this.entityInformation = (JpaEntityInformation<T, ID>) entityInformation;
     }
 
     @Override
     public <S> Optional<S> findByIdProjection(ID id, Class<S> projectionClass) {
-        String idField = findIdFieldName(domainClass);
-
+        String idField = idFieldName();
         Specification<T> spec = (root, query, cb) -> cb.equal(root.get(idField), id);
-
         return findBy(spec, q -> q.as(projectionClass).first());
     }
 
-    private String findIdFieldName(Class<?> clazz) {
-        for (Field field : clazz.getDeclaredFields()) {
-            if (field.isAnnotationPresent(Id.class)) {
-                return field.getName();
-            }
-        }
-        throw new IllegalStateException("No @Id field found in entity: " + clazz.getName());
+
+    @NonNull
+    @Override
+    public Class<T> domainType() {
+        return this.domainClass;
     }
 
+    @Override
+    @NonNull
+    public Class<ID> idType() {
+        return entityInformation.getIdType();
+    }
+
+    @Override
+    @NonNull
+    public String idFieldName() {
+        return entityInformation.getIdAttributeNames()
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("No @Id field found"));
+    }
 
 }
